1ff5053e036035c0dd7a1b1ea4938086639102a6
hb9397 <hb9397@naver.com>
2022-12-15 00:04:29 +0900

    BitLogicalOperator.java 코드수정(집) 20221214

---
+++ javaprac2/src/kakao/hb9397/operator/BitLogicalOperator.java
@@ -0,0 +1,11 @@
+package kakao.hb9397.operator;
+
+public class BitLogicalOperator {
+    public static void main(String[] args) {
+        int n1 = 20;
+        int n2 = 17;
+        System.out.println(n1 & n2); //16
+        System.out.println(n1 | n2); //21
+        System.out.println(n1 ^ n2); //5
+    }
+}
